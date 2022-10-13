c082b5ce7ec8d1604852ab507e2d9586894f4272
gaspar.kritsch <gaspar.kritsch.dev@gmail.com>
2022-10-13 18:28:43 -0300

    componente Item creado

--- src/components/CategoryContainer.js
+++
@@ -1,3 +0,0 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,22 +1,11 @@
-import ItemDetailContainer from "./ItemDetailContainer";
 import { Link } from "react-router-dom"
+import Item from "./Item";
 
 const ItemList = ({vehicles}) => {
-
-    console.log(vehicles)
-
+    
     return (vehicles.map(vehicle => (
-        <Link to={`/item/${vehicle.id}`} element={<ItemDetailContainer />} key={vehicle.id}>
-            <div className="vehicle">
-                <div className="vehicleImg">
-                    <img src={vehicle.img} alt={vehicle.img} />
-                </div>
-                <div className="vehicleInfo">
-                    <p className="precio">u$s {vehicle.precio}</p>
-                    <p className="titulo">{`${vehicle.marca} ${vehicle.modelo}`}</p>
-                    <p className="kilometros">{`${vehicle.kilometros} km.`}</p>
-                </div>
-            </div>
+        <Link to={`/item/${vehicle.id}`} key={vehicle.id}>
+            <Item vehicle = {vehicle}/>
         </Link>
     )))
 
